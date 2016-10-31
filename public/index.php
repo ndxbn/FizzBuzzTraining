f3542ac0986c13a60f4e2901351e62b8dc881ada
sitri <wc348nka@gmail.com>
2016-10-31 19:56:18 +0900

    Q2 3の倍数なら｢Fizz｣ 5の倍数なら｢Buzz｣ 両方の倍数なら｢FizzBuzz｣
    http://qiita.com/tadsan/items/b9521f149d4964b506d0#%E8%B3%AA%E5%95%8F2-3%E3%81%AE%E5%80%8D%E6%95%B0%E3%81%AA%E3%82%89fizz-5%E3%81%AE%E5%80%8D%E6%95%B0%E3%81%AA%E3%82%89buzz-%E4%B8%A1%E6%96%B9%E3%81%AE%E5%80%8D%E6%95%B0%E3%81%AA%E3%82%89fizzbuzz

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
 
 for ($i = 1; $i <= 100; ++$i) {
-    echo $i.PHP_EOL;
+    $unlessFzzBuzz = true;
+    if ($i % 3 === 0) {
+        echo 'Fizz';
+        $unlessFzzBuzz = false;
+    }
+    if ($i % 5 === 0) {
+        echo 'Buzz';
+        $unlessFzzBuzz = false;
+    }
+    if ($unlessFzzBuzz) {
+        echo $i;
+    }
+    echo PHP_EOL;
 }
