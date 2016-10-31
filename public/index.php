fecda79fd573c73e858195c6159385c5d8f309f3
sitri <wc348nka@gmail.com>
2016-10-31 20:48:10 +0900

    switch formatter by "mode" HTTP GET parameter。

--- public/index.php
+++ public/index.php
@@ -2,7 +2,24 @@
 
 require_once '../init.php';
 
-for ($i = 1; $i <= 100; ++$i) {
-    echo toFizzBuzz($i);
-    echo PHP_EOL;
+const MODE_CLI = 0;
+const MODE_HTML_UNORDERED_LIST = 1;
+
+/** @var int $mode */
+$mode = filter_input(INPUT_GET, 'mode', FILTER_VALIDATE_INT);
+if (false === $mode) {
+    $mode = 0;
 }
+// initialize formatter
+/** @var AbstractFormatter $formatter */
+$formatter = new ConsoleFormatter();
+if (MODE_HTML_UNORDERED_LIST === $mode) {
+    $formatter = new UnorderedListAbstractFormatter();
+}
+
+$outputBuffer = '';
+for ($i = 1; $i <= 100; ++$i) {
+    $outputBuffer .= $formatter->format(toFizzBuzz($i));
+}
+
+echo $outputBuffer;
